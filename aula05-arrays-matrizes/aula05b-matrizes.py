f81ddcc8b8ac42f91de20d50d0ba44d0af602e86
alexandrerussi <alexandrerussijunior@gmail.com>
2026-05-08 11:23:05 -0300

    aula 5

---
+++ aula05-arrays-matrizes/aula05b-matrizes.py
@@ -0,0 +1,13 @@
+musicas = [
+    ["Chicago", "Michael Jackson"],
+    ["Sorry", "Justin Bieber"],
+    ["Judas", "Lady Gaga"]
+]
+# print(musicas[1][0])
+
+for musica in musicas:
+
+    # print(f"{musica[0]} - {musica[1]}")
+    for info in musica:
+        print(info)
+    print()
